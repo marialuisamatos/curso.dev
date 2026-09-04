e00959cd2b6567b5908d54214a54dbe290dcf74b
Luisa <luisamatosisa@gmail.com>
2026-09-04 11:56:50 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>"Era uma pessoa igual a cem mil outras. Mas, eu fiz dela um amigo, agora ela é única no mundo." - O Pequeno Príncipe</h1>
+    return <h1>"Era uma pessoa igual a cem mil outras. Mas, eu fiz dela um amigo, agora ela é única no mundo." - O Pequeno Príncipe 🚀</h1>
 }
 
 export default Home;
